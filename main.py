e49020bb3aaa25c62e4ab9bf1baad3a9d955b30f
LIznzn <ww@acgmiao.com>
2021-10-19 15:58:53 +0900

    4

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.firefox.options import Options
 
 driver = webdriver.Firefox()
-wait = WebDriverWait(driver, 10)
 firefox_options = Options()
 firefox_options.set_preference('detach', True)
+
+# 登录
 driver.get("https://linkcom.muroran-it.ac.jp")
 driver.find_element(By.NAME, "LoginUserID").send_keys("15999833")
 driver.find_element(By.NAME, "LoginPassword").send_keys("15999833")
 driver.find_element(By.NAME, "LoginBtn").click()
 
-driver.find_element(By.LINK_TEXT, "伝言メモ").click()
+time.sleep(10)
+
+# 查看邮件数量
+driver.navigate("https://linkcom.muroran-it.ac.jp/Next60/default.cfm?version=next&app_cd=14&fuseaction=kms")
+print(driver.find_element(By.XPATH, "LoginBtn").text)
+
+
+
 
 
 
