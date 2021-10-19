8c89dfda527d61e45d03d186ba3cfdbdf930dfc8
LIznzn <ww@acgmiao.com>
2021-10-19 15:21:49 +0900

    2

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.firefox.options import Options
 
-
-with webdriver.Firefox() as driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get("https://linkcom.muroran-it.ac.jp")
-    driver.find_element(By.NAME, "LoginUserID").send_keys("15999833")
-    driver.find_element(By.NAME, "LoginPassword").send_keys("15999833")
-    driver.find_element(By.NAME, "LoginBtn").click()
+driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 10)
+firefox_options = Options()
+firefox_options.set_preference('detach', True)
+driver.get("https://linkcom.muroran-it.ac.jp")
+driver.find_element(By.NAME, "LoginUserID").send_keys("15999833")
+driver.find_element(By.NAME, "LoginPassword").send_keys("15999833")
+driver.find_element(By.NAME, "LoginBtn").click()
